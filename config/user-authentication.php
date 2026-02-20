420529f9e027c176e2e57c50ac2c2f8048f24cf5
iMercy <mercybrightmassang@gmail.com>
2026-02-20 07:48:59 -0700

    chore: Minor fixes

--- config/user-authentication.php
+++ config/user-authentication.php
@@ -47,6 +47,6 @@ return [
 
     'email_restrictions' => [
         'mode' => env('USER_AUTH_EMAIL_RESTRICTION_MODE', null), // 'whitelist', 'blacklist', or null
-        'domains' => array_filter(explode(',', env('USER_AUTH_EMAIL_RESTRICTION_DOMAINS', ''))), // Comma-separated list of domains
+        'domains' => array_filter(explode(',', env('USER_AUTH_EMAIL_RESTRICTION_DOMAINS', '')), 'strlen'), // Comma-separated list of domains
     ],
 ];
