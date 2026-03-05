5d135dd2a254f8f2d048f16fc620c83936fb2d1a
iMercy <mercybrightmassang@gmail.com>
2026-03-04 22:32:22 -0500

    chore: Resolving conflicts and failing checks

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -46,23 +46,16 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function getPackageProviders($app)
     {
         return [
-<<<<<<< HEAD
             'Whilesmart\UserAuthentication\UserAuthenticationServiceProvider',
             SocialiteServiceProvider::class,
 
         ];
     }
 
-=======
-            \Whilesmart\UserAuthentication\UserAuthenticationServiceProvider::class,
-            SocialiteServiceProvider::class,
-        ];
-    }
 
     /**
      * Get package aliases.
      */
->>>>>>> b9530bc (chore: Minor fixes)
     protected function getPackageAliases($app)
     {
         return [
