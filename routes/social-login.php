352122fe8f9f64369704754c86d0cd7305f4a648
Kofi Mokome <kofimokome10@gmail.com>
2026-05-06 10:32:27 +0000

    chore: Add post method to oauth callback endpoint

--- routes/social-login.php
+++ routes/social-login.php
@@ -6,4 +6,5 @@ use Whilesmart\UserAuthentication\Http\Controllers\Auth\AuthController;
 // OAUTH
 Route::get('/oauth/{driver}/login', [AuthController::class, 'oauthLogin']);
 Route::get('/oauth/{driver}/callback', [AuthController::class, 'oauthCallback']);
+Route::post('/oauth/{driver}/callback', [AuthController::class, 'oauthCallback']);
 Route::post('/oauth/firebase/{driver}/callback', [AuthController::class, 'firebaseAuthCallback']);
