c3f8f02c7457ee85df92a1a9118d6d5ecdf99652
Kofi <kofimokome10@gmail.com>
2026-05-27 23:52:19 +0100

    feat: Add passkey feature

---
+++ src/Models/Passkey.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Whilesmart\UserAuthentication\Models;
+
+use Cose\Algorithm\Manager;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Symfony\Component\Serializer\SerializerInterface;
+use Webauthn\AttestationStatement\AttestationStatementSupportManager;
+use Webauthn\AttestationStatement\FidoU2FAttestationStatementSupport;
+use Webauthn\AttestationStatement\NoneAttestationStatementSupport;
+use Webauthn\AttestationStatement\PackedAttestationStatementSupport;
+use Webauthn\CredentialRecord;
+use Webauthn\Denormalizer\WebauthnSerializerFactory;
+
+class Passkey extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'credential_id',
+        'data',
+        'user_id',
+        'keyable_type',
+        'keyable_id',
+    ];
+
+    public function keyable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function getCredential(): CredentialRecord
+    {
+
+        return $this->webAuthnSerializer()->deserialize(
+            $this->data,
+            CredentialRecord::class,
+            'json'
+        );
+
+    }
+
+    public static function webAuthnSerializer(): SerializerInterface
+    {
+        $attestationStatementSupportManager = new AttestationStatementSupportManager();
+        $attestationStatementSupportManager->add(new NoneAttestationStatementSupport()); // none is the recommended default
+        $serializers = config('user-authentication.passkey.serializers');
+        if (in_array('packed', $serializers, true)) {
+            $manager = new Manager();
+            $attestationStatementSupportManager->add(new PackedAttestationStatementSupport($manager));
+        }
+        if (in_array('fido', $serializers, true)) {
+            $attestationStatementSupportManager->add(new FidoU2FAttestationStatementSupport());
+        }
+
+        return (new WebauthnSerializerFactory($attestationStatementSupportManager))->create();
+    }
+}
